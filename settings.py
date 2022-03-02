c1bbce13a81f8147f6745a16c07f8fd8f8d6b183
mwalton16 <Max_Walton@baylor.edu>
2022-03-01 20:58:18 -0600

    Chapter 13 Complete

--- settings.py
+++ settings.py
@@ -4,10 +4,16 @@ class Settings:
         self.screen_width = 1200
         self.screen_height = 800
         self.ship_speed = 1.5
+        self.ship_limit = 3
         self.bg_color = (230,230,230)
     ### Bullet Attributes
-        self.bullet_speed = 1.0
+        self.bullet_speed = 1.5
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60,60,60)
         self.bullets_allowed = 3
+    ### Alien Attributes
+        self.alien_speed = 1.0
+        self.fleet_drop_speed = 10
+        self.fleet_direction = 1
+        ### Fleet direction of 1 represents right, -1 represents left
