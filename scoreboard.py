8ae07749c3cbd0790ae4fe1bd16903148a9f5a7b
mwalton16 <Max_Walton@baylor.edu>
2022-03-02 10:59:47 -0600

    Aesthetic Changes

--- scoreboard.py
+++ scoreboard.py
@@ -19,7 +19,7 @@ class Scoreboard:
 
     def prep_score(self):
         rounded_score = round(self.stats.score,-1)
-        score_str = "{:,}".format(rounded_score)
+        score_str = "Score: "+"{:,}".format(rounded_score)
         self.score_image = self.font.render(score_str,True,self.text_color,self.settings.bg_color)
         self.score_rect = self.score_image.get_rect()
         self.score_rect.right = self.screen_rect.right - 20
@@ -27,14 +27,14 @@ class Scoreboard:
 
     def prep_high_score(self):
         high_score = round(self.stats.high_score,-1)
-        high_score_str = "{:,}".format(high_score)
+        high_score_str = "High Score: "+"{:,}".format(high_score)
         self.high_score_image = self.font.render(high_score_str,True,self.text_color,self.settings.bg_color)
         self.high_score_rect = self.high_score_image.get_rect()
         self.high_score_rect.centerx = self.screen_rect.centerx
         self.high_score_rect.top = self.score_rect.top
 
     def prep_level(self):
-        level_str = str(self.stats.level)
+        level_str = "Level: "+str(self.stats.level)
         self.level_image = self.font.render(level_str,True,self.text_color,self.settings.bg_color)
         self.level_rect = self.level_image.get_rect()
         self.level_rect.right = self.score_rect.right
